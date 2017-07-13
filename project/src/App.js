f53e5b167eb118dc7cd09d48a988969bb7a67dda
Carter Hatch <carterhatch@live.com>
2017-07-13 17:49:42 -0600

    getting going

--- project/src/App.js
+++ project/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        
+        <div className="title-bubble">
+          <h1 className="main-title"> React Quest! </h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
